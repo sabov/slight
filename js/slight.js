459299a55b2a763cb7fdfac0de35e073f4b1f51a
sabov <alexsabov@gmail.com>
2013-05-01 18:00:34 +0300

    Add slight class and helpers functions

--- js/slight.js
+++ js/slight.js
@@ -1,3 +1,54 @@
 (function() {
-    alert('work!');
+
+    var translate = function (t) {
+        return " translate3d(" + t.x + "px," + t.y + "px," + t.z + "px) ";
+    };
+    var rotate = function (r) {
+        return " rotateX(" + r.x + "deg) ";
+    };
+
+    var scale = function (s) {
+        return " scale(" + s + ") ";
+    };
+
+    var Slight = function(el) {
+        this.$el = $(el);
+        this.$slides = this.$el.find('.slide');
+        console.log(this.$el);
+        this.initSlides();
+    };
+
+    Slight.prototype = {
+        initSlides: function() {
+            this.$slides.each(function(index, slide) {
+                this.showSlide(slide);
+            }.bind(this));
+        },
+        showSlide: function(slide) {
+            console.log(slide);
+            $(slide).css({
+                transform: translate({
+                    x: 50,
+                    y: 50,
+                    z: 0
+                }) + scale(0.5),
+                opacity: '1'
+            });
+        },
+        moveTo: function(index) {
+
+        },
+        shift: function(shift) {
+
+        },
+        next: function() {
+            this.shift(1);
+        },
+        prev: function() {
+            this.shift(-1);
+        }
+    };
+    $(function() {
+        new Slight('.slides');
+    });
 })();

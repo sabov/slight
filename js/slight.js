e856aeb6fe672dbdebd9c3afc41073a7f2641fcc
sabov <alexsabov@gmail.com>
2013-05-05 20:55:05 +0300

    Change angle for slides

--- js/slight.js
+++ js/slight.js
@@ -4,7 +4,7 @@
         return " translate3d(" + t.x + "px," + t.y + "px," + t.z + "px) ";
     };
     var rotate = function (r) {
-        return " rotateX(" + r.x + "deg) ";
+        return " rotate(" + r + "deg) ";
     };
 
     var scale = function (s) {
@@ -23,10 +23,8 @@
     Slight.prototype = {
         initSlides: function() {
             this.setPosition();
-            var length = this.$slides.length;
             this.$slides.each(function(index, slide) {
-                console.log([slide, length-index]);
-                this.showSlide(slide, length - index);
+                this.showSlide(slide, index);
             }.bind(this));
         },
         setPosition: function() {
@@ -44,48 +42,48 @@
             $(window).resize(function() {
                 if(tId) window.clearTimeout(tId);
                 tId = window.setTimeout(function() {
-                    self.initSlides();
+                    self.setPosition();
+                    self.moveTo(self.currSlide);
                 }, 300);
             });
 
         },
         showSlide: function(slide, index) {
+            var angle = index == this.currSlide? 0 : Math.floor((Math.random()*10)-6);
             var p = this.position;
             $(slide).css({
                 transform: 'translate(-50%, -50%)' +
                     translate({
                         x: p.left,
                         y: p.top,
-                        z: index
-                    }) + scale(p.scale),
+                        z: 0
+                    }) + scale(p.scale) +
+                    rotate(angle),
                 opacity: '1',
-                'z-index': index
+                'z-index': this.$slides.length - index
             });
         },
         hideSlide: function(slide) {
+            var p = this.position;
             $(slide).css({
-                transform: translate({
-                    x: 2500,
-                    y: 0,
-                    z: 0
-                }) + scale(0.5),
+                transform: 'translate(-50%, -50%)' +
+                    translate({
+                        x: 2500,
+                        y: p.top,
+                        z: 0
+                    }) + scale(p.scale),
                 opacity: '0'
             });
         },
         moveTo: function(index) {
-            var slideHeight = this.$slides.first().outerHeight();
-            var windowHeight = window.innerHeight;
-            var windowWidth = window.innerWidth;
-            var slideScale = windowHeight/slideHeight;
-            this.$slides.each(function(index, slide) {
-                if(this.currSlide <= index) {
-                    this.showSlide(slide, slideScale- 0.1, windowWidth/2, windowHeight/2);
-                } else {
+            this.currSlide = index;
+            this.$slides.each(function(i, slide) {
+                if(i < index) {
                     this.hideSlide(slide);
+                } else {
+                    this.showSlide(slide, i);
                 }
             }.bind(this));
-            this.currSlide = index;
-
         },
         shift: function(shift) {
             this.moveTo(this.currSlide + shift);
@@ -94,7 +92,6 @@
             this.shift(1);
         },
         prev: function() {
-            console.log('prev');
             this.shift(-1);
         }
     };
